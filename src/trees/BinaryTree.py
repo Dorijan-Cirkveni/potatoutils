546f03e80b0942abdacd87041fce7e9a073d1a7c
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-02-15 22:42:42 +0100

    Started project
    -Set up the whole thing
    -Added base for binary tree

---
+++ src/trees/BinaryTree.py
@@ -0,0 +1,16 @@
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        return
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root=None
+        return
+    def insert(self,value):
+        return
+    def remove(self,value):
+        return
