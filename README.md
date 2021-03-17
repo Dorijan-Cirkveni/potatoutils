b2d0e228f950cbfb5a85f6387379cdc86aaa459b
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-03-17 17:15:53 +0100

    Y E E T

--- README.md
+++
@@ -1,2 +0,0 @@
-# potatoutils
-Attempting to make this library work was an utterly exhausting endeavour and I give up.
--- src/helloworld.py
+++
@@ -1,6 +0,0 @@
-def fucking_work_goddamnit():
-    print("Fucking finally")
-    return
-
-if __name__ == "__main__":
-    print("Hello World")
