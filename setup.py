68c3d6394b8154c13cb6b9707816d626ff719e81
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-03-17 23:23:31 +0100

    It's working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='potatoutils',
     packages=find_packages(),
-    version='0.0.2',
+    version='0.0.3',
     description='My first Python library',
     author='Dorijan Cirkveni',
     license='MIT',
